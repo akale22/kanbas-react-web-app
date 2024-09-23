7fecca9eaa75dddd0a49ec9e5e7c3475a497a01e
Aryan Kale <aryankale22@gmail.com>
2024-09-22 22:49:56 -0400

    Finishing a1 boilerplate

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Branch for Assignment 1
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import Labs from "./Labs";
+import Kanbas from "./Kanbas";
+import { Navigate, Route, Routes } from "react-router";
+import { HashRouter } from "react-router-dom";
+export default function App() {
+ return (
+  <HashRouter>
+   <div>
+    <Routes>
+     <Route path="/" element={<Navigate to="Kanbas"/>}/>
+     <Route path="/Labs/*" element={<Labs />} />
+     <Route path="/Kanbas/*" element={<Kanbas />} />
+    </Routes>
+   </div>
+  </HashRouter>
+);}
