24cdb3b6e59a0ae90e8a31f3cff98d9b8bd7c5d4
Aryan Kale <aryankale22@gmail.com>
2024-10-09 00:53:25 -0400

    Finished 4.9

--- src/Kanbas/Account/Navigation.tsx
+++ src/Kanbas/Account/Navigation.tsx
@@ -1,9 +1,31 @@
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router";
+
 export default function AccountNavigation() {
+  const { pathname } = useLocation();
   return (
-    <div id="wd-account-navigation">
-      <Link to={`/Kanbas/Account/Signin`}  > Signin  </Link> <br/>
-      <Link to={`/Kanbas/Account/Signup`}  > Signup  </Link> <br/>
-      <Link to={`/Kanbas/Account/Profile`} > Profile </Link> <br/>
+    <div
+      id="wd-account-navigation"
+      className="wd list-group fs-5 rounded-0 pe-3"
+    >
+      <Link
+        to={`/Kanbas/Account/Signin`}
+        className={`list-group-item ${pathname.includes("Signin") ? "active text-black" : "text-danger"} border border-0`}
+      >
+        Signin
+      </Link>
+      <Link
+        to={`/Kanbas/Account/Signup`}
+        className={`list-group-item ${pathname.includes("Signup") ? "active text-black" : "text-danger"} border border-0`}
+      >
+        Signup
+      </Link>
+      <Link
+        to={`/Kanbas/Account/Profile`}
+        className={`list-group-item ${pathname.includes("Profile") ? "active text-black" : "text-danger"} border border-0`}
+      >
+        Profile
+      </Link>
     </div>
-);}
+  );
+}
--- src/Kanbas/Account/Profile.tsx
+++ src/Kanbas/Account/Profile.tsx
@@ -1,21 +1,55 @@
 import { Link } from "react-router-dom";
+
 export default function Profile() {
   return (
     <div id="wd-profile-screen">
       <h3>Profile</h3>
-      <input defaultValue="alice" placeholder="username" className="wd-username"/><br/>
-      <input defaultValue="123"   placeholder="password" type="password"
-             className="wd-password" /><br/>
-      <input defaultValue="Alice" placeholder="First Name" id="wd-firstname" /><br/>
-      <input defaultValue="Wonderland" placeholder="Last Name" id="wd-lastname" /><br/>
-      <input defaultValue="2000-01-01" type="date" id="wd-dob" /><br/>
-      <input defaultValue="alice@wonderland" type="email" id="wd-email" /><br/>
-      <select defaultValue="FACULTY" id="wd-role">
+      <input
+        className="form-control mb-2"
+        id="wd-username"
+        value="alice"
+        placeholder="username"
+      />
+      <input
+        className="form-control mb-2"
+        id="wd-password"
+        value="123"
+        placeholder="password"
+        type="password"
+      />
+      <input
+        className="form-control mb-2"
+        id="wd-firstname"
+        value="Alice"
+        placeholder="First Name"
+      />
+      <input
+        className="form-control mb-2"
+        id="wd-lastname"
+        value="Wonderland"
+        placeholder="Last Name"
+      />
+      <input
+        className="form-control mb-2"
+        id="wd-dob"
+        value="2000-01-01"
+        type="date"
+      />
+      <input
+        className="form-control mb-2"
+        id="wd-email"
+        value="alice@wonderland"
+        type="email"
+      />
+      <select className="form-control mb-2" id="wd-role">
         <option value="USER">User</option>
         <option value="ADMIN">Admin</option>
         <option value="FACULTY">Faculty</option>
         <option value="STUDENT">Student</option>
-      </select><br/>
-      <Link to="/Kanbas/Account/Signin" >Sign out</Link>
+      </select>
+      <Link to="/Kanbas/Account/Signin" className="btn btn-danger w-100">
+        Sign out
+      </Link>
     </div>
-);}
+  );
+}
