321129acbc2992b9a65e4241bf5f2477874b84b0
Aryan Kale <aryankale22@gmail.com>
2024-10-08 22:21:29 -0400

    Small style stuff for 4.4 and 4.5

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -6,7 +6,7 @@ import { BsGripVertical } from "react-icons/bs";
 
 export default function Modules() {
   return (
-    <div>
+    <div className="me-4">
   <ModulesControls /><br /><br /><br /><br />
   <ul id="wd-modules" className="list-group rounded-0">
     <li className="wd-module list-group-item p-0 mb-5 fs-5 border-gray">
