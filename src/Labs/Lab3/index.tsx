8493bda918e940f3544223bad413308fa770deec
Aryan Kale <aryankale22@gmail.com>
2024-10-27 15:53:01 -0400

    finished labs

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -31,12 +31,22 @@ import TodoItem from "./todos/TodoItem";
 import TodoList from "./todos/TodoList";
 import VariablesAndConstants from "./VariablesAndConstants";
 import VariableTypes from "./VariableTypes";
+import { useSelector } from "react-redux";
 
 export default function Lab3() {
-  console.log('Hello World!');
+  const { todos } = useSelector((state: any) => state.todosReducer);
   return (
     <div id="wd-lab3" className="container">
       <h3>Lab 3</h3>
+      <ul className="list-group">
+        {todos.map((todo: any) => (
+          <li className="list-group-item" key={todo.id}>
+            {todo.title}
+          </li>
+        ))}
+      </ul>
+      <hr />
+
       <VariablesAndConstants />
       <VariableTypes />
       <BooleanVariables />
@@ -57,13 +67,13 @@ export default function Lab3() {
       <FindIndex />
       <FilterFunction />
       <JsonStringify />
-      <House />  
+      <House />
       <TodoItem />
       <TodoList />
       <Spreading />
       <Destructing />
       <FunctionDestructing />
-      <DestructingImports /> 
+      <DestructingImports />
       <Classes />
       <Styles />
       <Add a={3} b={4} />
@@ -71,12 +81,12 @@ export default function Lab3() {
       <Square>4</Square>
       <hr />
       <Highlight>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
-        vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
-     </Highlight>
-     <PathParameters />
-
-
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione
+        eaque illo minus cum, saepe totam vel nihil repellat nemo explicabo
+        excepturi consectetur. Modi omnis minus sequi maiores, provident
+        voluptates.
+      </Highlight>
+      <PathParameters />
     </div>
   );
 }
